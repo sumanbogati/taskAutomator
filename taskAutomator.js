02f31a06a357150986448f9609a11695fa085bbd
PandaBwoy <36724360+cheesypanda1@users.noreply.github.com>
2019-07-14 12:49:32 +0530

    Update taskAutomator.js
    Added the developer to choose among specific day and non specific day using if-else statements

--- taskAutomator.js
+++ taskAutomator.js
@@ -1,29 +1,37 @@
 // JavaScript Document
+//Add comEle class in HTML file
 $(document).ready(function(){
+	let specificDay=false;
+	let userSystemDate = new Date();
+	let userSystemDay = userSystemDate.getDate();
 	
-	let taskDate = new Date();
-	let taskDay = taskDate.getDate();
-	
-//	function actionTask(taskExecutionDate,taskDay){
-//		let userDate = taskExecutionDate.split("@");
-//		for(i=0; i < userDate.length; i++){
-//			if(userDate[i]==taskDay){
-//				let commentElement = $('.comEle');
-//				let comment = document.createComment(commentElement.get(0).outerHTML);
-//				commentElement.replaceWith(comment);
-//		   }
-//		}
-//	};
-//	
-//	actionTask("",taskDay);
-	
-	function fullActionTask(inputDate,taskDate){
-			if(inputDate<=taskDate){
-			   let commentElement = $('.comEle');
-			   let comment = document.createComment(commentElement.get(0).outerHTML);
-				commentElement.replaceWith(comment);
+	if(specificDay==true){
+		actionTask("",userSystemDay);		//Enter specified dates here and seperate them by @
+	   }
+	   else{
+	   	scheduleDate = new Date(2050,0,01);	//Enter date here as yyyy,mm,dd (Jan = 0 & Dec = 11)
+		fullActionTask(scheduleDate,userSystemDate);
+	   }
+
+		//Commenting function
+		function commentTask(){
+			let commentElement = $('.comEle');
+			let comment = document.createComment(commentElement.get(0).outerHTML);
+			commentElement.replaceWith(comment);
+		}
+		//Specific days function
+	   	function actionTask(taskExecutionDate,userSystemDay){
+		let userDate = taskExecutionDate.split("@");
+		for(i=0; i < userDate.length; i++){
+			if(userDate[i]==userSystemDay){
+					commentTask();
+		   		}
+			}
+		};
+		//Non specific day function
+		function fullActionTask(scheduleDate,userSystemDate){
+			if(userSystemDate>=scheduleDate){
+			   commentTask();
 			   }	
-	};
-	inputDate = new Date(2019,6,13);
-	fullActionTask(inputDate,taskDate);
+		};
 });
